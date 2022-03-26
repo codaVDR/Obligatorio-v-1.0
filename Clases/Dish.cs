c56056721d9ad088358452fedef91130ab2f3385
Sintesis <iribastrillo@gmail.com>
2022-03-26 17:29:13 -0300

    ADD: define propiedades en Dish

--- Clases/Dish.cs
+++ Clases/Dish.cs
@@ -4,10 +4,36 @@ namespace Dominio
 {
     public class Dish
     {
+        static public float minimumPrice = 100;
         static public int id = 0;
         private int ID = 0;
         private string name;
         private float price;     
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                //Falta implementar
+            }
+        }
+
+        public float Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                //Falta implementar
+            }
+        }
 
         public Dish (string name, float price)
         {
