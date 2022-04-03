30b49de2a656d76cb7ee3682bf67dfe5ee451995
Sintesis <iribastrillo@gmail.com>
2022-04-03 13:41:37 -0300

    ADD: implementa enum Vehicle para vehículos en Deliveryman

--- Dominio/Deliveryman.cs
+++ Dominio/Deliveryman.cs
@@ -5,12 +5,18 @@ namespace Dominio
 {
     public class Deliveryman : Person
     {
-        public string name;
-        public string last_name;
-
-        public Deliveryman(string name, string last_name) : base (name, last_name)
+        private Vehicle vehicle;
+        public Deliveryman(string name, string last_name, Vehicle vehicle) : base (name, last_name)
         {
-            //Acá debería implementarse la asignación de un vehículo para este objeto.
+            Vehicle = vehicle;
         }
+        public Vehicle Vehicle { get => vehicle; set => vehicle = value; }
+    }
+
+    public enum Vehicle
+    {
+        MOTO,
+        BICICLETA,
+        PIE
     }
 }
