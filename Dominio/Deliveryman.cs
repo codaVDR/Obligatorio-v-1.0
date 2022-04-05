478f6be7af40ef0cb9b47ecfaa6373b08ba67e8c
Agustina Balsas <ab212839@fi365.ort.edu.uy>
2022-04-05 17:19:09 -0300

    otro fix

--- Dominio/Deliveryman.cs
+++ Dominio/Deliveryman.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 
 
+
 namespace Dominio
 {
     public class Deliveryman : Person
     {
         public string name;
         public string last_name;
+        public Dominio.Enums.TipoVehiculo Tipo { get; set; }
+
 
         public Deliveryman(string name, string last_name) : base (name, last_name)
         {
+            
             //Acá debería implementarse la asignación de un vehículo para este objeto.
         }
     }
